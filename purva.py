1e2dff2a35a6b2d309e2290de179b6350a5c4360
PURVA KADU <kadu@gmail.com>
2019-12-29 06:44:25 +0000

    New update from Dev 1

--- purva.py
+++ purva.py
@@ -1,3 +1,16 @@
 
-Print{Welcome to Pune}
 
+read -p "enter three numbers:" N1 N2 N3
+echo "$N1 $N2 $N3"
+if [[ $N1 -gt $N2 && $N1 -gt $N3 ] && [ $N2 -gt $N3 ]]
+then
+echo "$N1 is Greater"
+elif
+echo "wrong input"
+fi
+
+====================================
+
+
+Print "Welcome to Pune"
+Print "Welcome Purva Kadu"

ccbac0b48976d9dd18d2e52d29930beda2204083
PURVA KADU <kadu@gmail.com>
2019-12-29 06:17:16 +0000

    Latest update

--- calc.py
+++ calc.py
@@ -1,12 +1,11 @@
 
 def add(x,y):
-    pass
-def subtract(x,y):
-    pass
-def multiply(x,y):
-    pass
-def divide(x,y):
-    pass
-def square(x,y):
-    pass
 
+def subtract(x,y):
+    
+def multiply(x,y):
+    
+def divide(x,y):
+
+def square(x,y):
+    

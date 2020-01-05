361f36c222ce7b59fa77da037c353b608009da0e
PURVA KADU <kadu@gmail.com>
2020-01-05 10:55:38 +0000

    New commit

--- calc.py
+++ calc.py
@@ -1,14 +1,15 @@
 
+
+
 def add(x,y):
-
+    pass
 def subtract(x,y):
-    
+    abcdefff
+     ghjkkkkkkss
 def multiply(x,y):
-    
+    pass
 def divide(x,y):
-
+    pass
 def square(x,y):
+    pass
 
-making new changes
-
-def addition    
